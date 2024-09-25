7a52be4e72bec8580f875d0c4daf70b9132c7129
Jakob Frick <jakob.maria.frick@gmail.com>
2024-09-25 16:37:33 -0700

    enable telemtry

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,14 +1,19 @@
 import { registerOTel } from "@vercel/otel";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
 
 export function register() {
   registerOTel({
     serviceName: "hunter-game",
-    traceExporter: new OTLPTraceExporter({
-      url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-      headers: {
-        Authorization: `Bearer ${process.env.OTEL_EXPORTER_OTLP_AUTH_TOKEN}`,
-      },
-    }),
+    spanProcessors: [
+      new SimpleSpanProcessor(
+        new OTLPTraceExporter({
+          url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+          headers: {
+            Authorization: `Bearer ${process.env.OTEL_EXPORTER_OTLP_AUTH_TOKEN}`,
+          },
+        })
+      ),
+    ],
   });
 }
