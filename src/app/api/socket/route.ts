3c60a553b29beb4db557798e99667d210efc00e1
Jakob Frick <jakob.maria.frick@gmail.com>
2025-05-22 15:47:41 -0700

    up

--- src/app/api/socket/route.ts
+++ src/app/api/socket/route.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest } from 'next';
-import { NextApiResponseServerIO } from '@/types/next';
+import { NextApiResponseWithSocket } from '@/app/api/socket/typings';
 import { initWebSocketServer } from '@/lib/websocket-server';
 
 export const config = {
@@ -9,7 +9,7 @@ export const config = {
 };
 
 // This handler ensures the WebSocket server is initialized with real game logic
-export default function handler(req: NextApiRequest, res: NextApiResponseServerIO) {
+export default function handler(req: NextApiRequest, res: NextApiResponseWithSocket) {
   if (!res.socket.server.io) {
     // Use the real game server logic
     initWebSocketServer(res.socket.server);
